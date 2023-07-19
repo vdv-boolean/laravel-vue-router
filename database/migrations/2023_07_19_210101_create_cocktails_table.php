892a3fd7283f2d8fa0de20d4d6e9e45cc95c6938
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-19 23:22:25 +0200

    add columns

--- database/migrations/2023_07_19_210101_create_cocktails_table.php
+++ database/migrations/2023_07_19_210101_create_cocktails_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('cocktails', function (Blueprint $table) {
             $table->id();
+            $table->string('strDrink',50);
+            $table->string('strDrinkThumb',200);
             $table->timestamps();
         });
     }
